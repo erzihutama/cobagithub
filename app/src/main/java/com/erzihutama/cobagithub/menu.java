ff9f24132e2e2d17ff9d39b42ebc40a7ae9d4c8c
Erzi Hutama Dwirama Putra <erzihutama@gmail.com>
2019-05-17 19:38:09 +0700

    menambah menu

---
+++ app/src/main/java/com/erzihutama/cobagithub/menu.java
@@ -0,0 +1,17 @@
+package com.erzihutama.cobagithub;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.erzihutama.cobagithub.R;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
